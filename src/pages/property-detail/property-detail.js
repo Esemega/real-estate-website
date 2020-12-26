a63aab6abc49ebfedf8f8743b1266e89d0c635bf
Esemega <sogaral89@gmail.com>
2020-12-26 12:51:25 +0100

    Obtaining the details of Property depending on url id param

--- src/pages/property-detail/property-detail.js
+++ src/pages/property-detail/property-detail.js
@@ -1,1 +1,12 @@
-console.log('Detail page')
+import { getPropertyById, getEquipments } from './property-detail.api';
+import { history } from '../../core/router';
+import { setPropertyValues } from './property-detail.helpers';
+import { mapPropertyFromApiToVm } from './property-detail.mappers';
+
+const { id } = history.getParams();
+
+Promise.all([getPropertyById(id), getEquipments()]).then(
+  ([property, equipments]) => {
+    setPropertyValues(mapPropertyFromApiToVm(property, equipments));
+  }
+);
