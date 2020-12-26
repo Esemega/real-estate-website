53a1df6812035ada76644192a8032246353815b5
Esemega <sogaral89@gmail.com>
2020-12-26 14:24:05 +0100

    Creating the contact data model, validating the form data and sending to server

--- src/pages/property-detail/property-detail.js
+++ src/pages/property-detail/property-detail.js
@@ -1,7 +1,22 @@
-import { getPropertyById, getEquipments } from './property-detail.api';
+import {
+  getPropertyById,
+  getEquipments,
+  sendContact,
+} from './property-detail.api';
 import { history } from '../../core/router';
 import { setPropertyValues } from './property-detail.helpers';
-import { mapPropertyFromApiToVm } from './property-detail.mappers';
+import {
+  mapPropertyFromApiToVm,
+  mapContactFromVmToApi,
+} from './property-detail.mappers';
+import {
+  onUpdateField,
+  onSubmitForm,
+  onSetError,
+  onSetFormErrors,
+  onSetValues,
+} from '../../common/helpers';
+import { formValidation } from './property-detail.validations';
 
 const { id } = history.getParams();
 
@@ -10,3 +25,55 @@ Promise.all([getPropertyById(id), getEquipments()]).then(
     setPropertyValues(mapPropertyFromApiToVm(property, equipments));
   }
 );
+
+let contact = {
+  email: '',
+  message: '',
+};
+
+const onUpdateAllFileds = () => {
+  Object.keys(contact).map((keyName) => {
+    onUpdateField(keyName, (event) => {
+      const value = event.target.value;
+      contact = {
+        ...contact,
+        [keyName]: value,
+      };
+
+      formValidation.validateField(keyName, contact[keyName]).then((result) => {
+        onSetError(keyName, result);
+      });
+    });
+  });
+};
+
+onUpdateAllFileds();
+
+const onContact = () => {
+  const apiContact = mapContactFromVmToApi(contact, id);
+  return sendContact(apiContact);
+};
+
+const resetContactData = () => {
+  contact = {
+    email: '',
+    message: '',
+  };
+  onSetValues(contact);
+};
+
+onSubmitForm('contact-button', () => {
+  formValidation.validateForm(contact).then((result) => {
+    onSetFormErrors(result);
+    if (result.succeeded) {
+      onContact().then((apiContact) => {
+        console.log({ contact });
+        console.log({ apiContact });
+        window.alert(
+          'Gracias por confiar en nosotros. Le contactaremos lo antes posible.'
+        );
+        resetContactData();
+      });
+    }
+  });
+});
